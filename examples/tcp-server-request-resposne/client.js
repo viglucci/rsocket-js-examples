0370ce40da804005622130dee07b090f1854971c
Kevin Viglucci <kviglucci@gmail.com>
2021-07-28 23:47:41 -0500

    refactor: implement routing via composite metadata

--- examples/tcp-server-request-resposne/client.js
+++ examples/tcp-server-request-resposne/client.js
@@ -1,25 +1,34 @@
-const {RSocketClient} = require('rsocket-core');
+const {
+  RSocketClient,
+  MESSAGE_RSOCKET_COMPOSITE_METADATA,
+  MESSAGE_RSOCKET_ROUTING,
+  BufferEncoders,
+  encodeCompositeMetadata,
+  encodeRoute
+} = require('rsocket-core');
 const RSocketTCPClient = require('rsocket-tcp-client').default;
 
-const requestInterval = 750;
-const maxTimeElapsed = 1000;
-
-function now() {
-  return (new Date()).getTime();
+function encodeMetadata({route}) {
+  return encodeCompositeMetadata([
+    [MESSAGE_RSOCKET_ROUTING, encodeRoute(route)],
+  ]);
 }
 
 function createClient(options) {
+
+  const transport = new RSocketTCPClient({
+    host: options.host,
+    port: options.port,
+  }, BufferEncoders);
+
   return new RSocketClient({
     setup: {
-      dataMimeType: 'application/json',
+      dataMimeType: 'application/octet-stream',
       keepAlive: 1000000,
       lifetime: 100000,
-      metadataMimeType: 'application/json',
+      metadataMimeType: MESSAGE_RSOCKET_COMPOSITE_METADATA.string,
     },
-    transport: new RSocketTCPClient({
-      host: options.host,
-      port: options.port,
-    }),
+    transport
   });
 }
 
@@ -29,15 +38,47 @@ async function connect(options) {
 }
 
 async function getCurrentTime(rsocket) {
+
   return new Promise(function (resolve, reject) {
+
+    const encodedMetadata = encodeMetadata({
+      route: "timeService.currentTime"
+    });
+
     const payload = {
       data: null,
-      // TODO: shouldn't need to manually stringify JSON object when using `application/json` mimetype
-      metadata: JSON.stringify({
-        messageId: "timeService.currentTime"
-      })
+      metadata: encodedMetadata
     };
-    console.log(`request: `, payload);
+
+    rsocket
+        .requestResponse(payload)
+        .subscribe({
+          onComplete: (response) => {
+            resolve(response);
+          },
+          onError: (error) => {
+            reject(error);
+          },
+          onSubscribe: (_cancel) => {
+            //...
+          },
+        });
+  });
+}
+
+async function getProduct(rsocket, { lhs, rhs }) {
+
+  return new Promise(function (resolve, reject) {
+
+    const encodedMetadata = encodeMetadata({
+      route: "multiplicationService.multiply"
+    });
+
+    const payload = {
+      data: Buffer.from(JSON.stringify({ lhs, rhs })),
+      metadata: encodedMetadata
+    };
+
     rsocket
         .requestResponse(payload)
         .subscribe({
@@ -55,18 +96,33 @@ async function getCurrentTime(rsocket) {
 }
 
 async function run() {
+
   const rsocket = await connect({
     host: '127.0.0.1',
     port: 9090,
   });
+
+  const getTimePromise = getCurrentTime(rsocket)
+      .then((response) => {
+        const time = response.data.toString();
+        console.log(`decoded getCurrentTime result: ${time}`);
+      })
+      .catch((error) => {
+        console.error(`getCurrentTime error: `, error);
+      });
+
+  const getProductPromise = getProduct(rsocket, { lhs: 2, rhs: 3 })
+      .then((response) => {
+        const time = response.data.toString();
+        console.log(`decoded getProduct result: ${time}`);
+      })
+      .catch((error) => {
+        console.error(`getProduct error: `, error);
+      });
+
   return Promise.all([
-    getCurrentTime(rsocket)
-        .then((response) => {
-          console.log(`response: `, response);
-        })
-        .catch((error) => {
-          console.error(`error: `, error);
-        })
+    getTimePromise,
+    getProductPromise
   ]);
 }
 
--- examples/tcp-server-request-resposne/server.js
+++ examples/tcp-server-request-resposne/server.js
@@ -1,6 +1,6 @@
-const { RSocketServer } = require('rsocket-core');
+const {RSocketServer, BufferEncoders, CompositeMetadata, MESSAGE_RSOCKET_ROUTING, decodeRoutes} = require('rsocket-core');
 const RSocketTCPServer = require('rsocket-tcp-server');
-const { Single } = require('rsocket-flowable');
+const {Single} = require('rsocket-flowable');
 
 const TCPTransport = RSocketTCPServer.default;
 
@@ -9,82 +9,146 @@ const transportOpts = {
   port: 9090,
 };
 
-const transport = new TCPTransport(transportOpts);
+const transport = new TCPTransport(transportOpts, BufferEncoders);
 
 const statuses = {
   PENDING: "pending",
   CANCELLED: "cancelled"
 };
 
-function handleCurrentTimeRequestResponse(payload) {
-  let status = statuses.PENDING;
+class TimeService {
+  currentTime(payload) {
+    let status = statuses.PENDING;
 
-  console.log(`requestResponse request`, payload);
+    console.log(`requestResponse request`, payload);
 
-  return new Single((subscriber) => {
-
-    /**
-     * In the event that the client cancels the request before
-     * the server can respond, we will change our status to cancelled
-     * and avoid calling `onComplete` on the `subscriber` instance in the
-     * `setTimeout` callback.
-     */
-    function handleCancellation() {
-      status = statuses.CANCELLED;
-    }
-
-    subscriber.onSubscribe(() => handleCancellation());
-
-    /**
-     * Leverage `setTimeout` to simulate a delay
-     * in responding to the client.
-     */
-    setTimeout(() => {
+    return new Single((subscriber) => {
 
       /**
-       * If the client cancelled the request we can
-       * return early and avoid doing any of the work below.
+       * In the event that the client cancels the request before
+       * the server can respond, we will change our status to cancelled
+       * and avoid calling `onComplete` on the `subscriber` instance in the
+       * `setTimeout` callback.
+       */
+      function handleCancellation() {
+        status = statuses.CANCELLED;
+      }
+
+      subscriber.onSubscribe(() => handleCancellation());
+
+      /**
+       * If the client cancelled the request before we got to this point,
+       * we can return early and avoid doing any of the work below.
        */
       if (status === statuses.CANCELLED) {
         return;
       }
 
       const msg = `${new Date()}`;
+
       console.log(`requestResponse response`, msg);
-      try {
-        subscriber.onComplete({
-          data: msg,
-          metadata: null, // or new Buffer(...)
-        });
-      } catch (e) {
-        subscriber.onError(e);
+
+      subscriber.onComplete({
+        data: Buffer.from(msg),
+        metadata: null, // or new Buffer(...)
+      });
+    });
+  }
+}
+
+class MultiplicationService {
+  multiply(args) {
+    console.log(`MultiplicationService.multiply`, args);
+
+    const { lhs, rhs } = args;
+
+    let status = statuses.PENDING;
+
+    return new Single((subscriber) => {
+
+      /**
+       * In the event that the client cancels the request before
+       * the server can respond, we will change our status to cancelled
+       * and avoid calling `onComplete` on the `subscriber` instance in the
+       * `setTimeout` callback.
+       */
+      function handleCancellation() {
+        status = statuses.CANCELLED;
       }
-    }, 100);
+
+      subscriber.onSubscribe(() => handleCancellation());
+
+      /**
+       * If the client cancelled the request before we got to this point,
+       * we can return early and avoid doing any of the work below.
+       */
+      if (status === statuses.CANCELLED) {
+        return;
+      }
+
+      const product = lhs * rhs;
+
+      console.log(`requestResponse response`, product);
+
+      subscriber.onComplete({
+        data: Buffer.from(JSON.stringify(product)),
+        metadata: null, // or new Buffer(...)
+      });
+    });
+  }
+}
+
+function handleUnsupportedMessageRequestResponse(route) {
+  return new Single((subscriber) => {
+    subscriber.onError(new Error("Unsupported route: " + route));
   });
 }
 
-function handleUnsupportedMessageRequestResponse(payload) {
-  return new Single((subscriber) => {
-    subscriber.onError(new Error("Unsupported messageId: " + payload.metadata));
-  });
+function unpackCompositeMetadata(metadata) {
+  const decodedMetadata = new CompositeMetadata(
+      metadata,
+  );
+
+  let unpacked = {
+    routes:  []
+  };
+
+  for (const metadataEntry of decodedMetadata) {
+    if (metadataEntry.mimeType === MESSAGE_RSOCKET_ROUTING.string) {
+      const routes = decodeRoutes(metadataEntry.content);
+      // routes is a generator that can technically produce
+      // more than a single value
+      for (const route of routes) {
+        unpacked.route = route;
+      }
+    }
+  }
+
+  return unpacked;
 }
 
 const getRequestHandler = (requestingRSocket, setupPayload) => {
 
   console.log("Client connected...", setupPayload);
 
+  const timeService = new TimeService();
+  const multiplicationService = new MultiplicationService();
+
   function handleRequestResponse(payload) {
 
     // TODO: shouldn't need to manually parse JSON object when using `application/json` mimetype
     const data = payload.data ? JSON.parse(payload.data) : null;
-    const { messageId } = JSON.parse(payload.metadata);
+    const { route } = unpackCompositeMetadata(payload.metadata);
 
-    switch (messageId) {
+    switch (route) {
       case 'timeService.currentTime': {
-        return handleCurrentTimeRequestResponse(data);
+        return timeService.currentTime(data);
+      }
+      case 'multiplicationService.multiply': {
+        return multiplicationService.multiply(data);
       }
       default: {
-        return handleUnsupportedMessageRequestResponse(data);
+        return handleUnsupportedMessageRequestResponse(route);
       }
     }
   }
